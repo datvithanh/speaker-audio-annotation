5466add3f516308750638451843913718f4f4187
Sieusaosieungo <thinhhanh1@gmail.com>
2020-12-07 19:06:13 +0700

    add fix average point

---
+++ scripts/20201307_fix_average_point.js
@@ -0,0 +1,35 @@
+require('../src/db/mongoose');
+const Audio = require('../src/models/audio.model');
+
+(async () => {
+  const audios = await Audio.find({
+    // test: { $in: ['5fcd0328a765493f035d186e', '5fcd1aa70705f2d8b5f42227'] }, // dev
+    test: { $in: ['5fc895cfd2eeea6aecd95bee', '5fc897bfd2eeea6aecd95dd0'] }, // production
+  }).lean();
+
+  await Promise.all(
+    audios.map(async audio => {
+      let numberOfReviews = 0;
+      let totalPoint = 0;
+      console.log({ test: audio.test, audio: audio._id });
+      audio.users.forEach(user => {
+        if (user.point) {
+          numberOfReviews += 1;
+          totalPoint += user.point;
+        }
+      });
+
+      let averagePoint = null;
+      if (numberOfReviews !== 0) {
+        averagePoint = totalPoint / numberOfReviews;
+      }
+
+      await Audio.findByIdAndUpdate(audio._id, {
+        averagePoint,
+        numberOfReviews,
+      });
+    }),
+  );
+
+  process.exit();
+})();
