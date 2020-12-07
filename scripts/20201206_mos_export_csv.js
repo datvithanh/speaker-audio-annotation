b7e99c5e1e6aba0b6b371319c5d4bffd34744d54
Pham Quang Minh <phamquangminh.hust@gmail.com>
2020-12-07 17:34:55 +0700

    fix script export vlsp final result 2020

--- scripts/20201206_mos_export_csv.js
+++ scripts/20201206_mos_export_csv.js
@@ -1,69 +1,62 @@
 /* eslint-disable no-console */
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+require('dotenv');
+const fs = require('fs');
+const moment = require('moment');
+
+require('../src/db/mongoose');
+
 const Audio = require('../src/models/audio.model');
 const User = require('../src/models/user.model');
 const Test = require('../src/models/test.model');
 const Sentence = require('../src/models/sentence.model');
-require('dotenv');
-
-require('../src/db/mongoose');
-
-const csvWriter = createCsvWriter({
-  path: 'src/static/mos.csv',
-  header: [
-    { id: '_id', title: 'ID' },
-    { id: 'test', title: 'test' },
-    { id: 'link', title: 'link' },
-    { id: 'voice', title: 'voice' },
-    { id: 'sentence', title: 'sentence' },
-    { id: 'createdAt', title: 'createdAt' },
-    { id: 'email', title: 'email' },
-    { id: 'name', title: 'name' },
-    { id: 'listens', title: 'listens' },
-    { id: 'point', title: 'point' },
-  ],
-});
 
 (async () => {
+  let data = [];
+
+  const firstMosId = '5fc895cfd2eeea6aecd95bee';
+  const secondMosId = '5fc897bfd2eeea6aecd95dd0';
+
+  const firstMosTest = await Test.findById(firstMosId);
+  const secondMosTest = await Test.findById(secondMosId);
+  
   const audios = await Audio.find({
-    // test: { $in: ['5fcd0328a765493f035d186e', '5fcd1aa70705f2d8b5f42227'] }, // dev
-    test: { $in: ['5fc895cfd2eeea6aecd95bee', '5fc897bfd2eeea6aecd95dd0'] }, // production
-  }).lean();
+    test: { $in: [firstMosId, secondMosId] },
+  })
+  console.log('audios.length', audios.length);
 
-  const data = [];
-  await Promise.all(
-    audios.map(async audio => {
-      await Promise.all(
-        audio.users.map(async user => {
-          const userObject = await User.findById(user.userId);
-          const testObject = await Test.findById(audio.test);
-          const sentenceObject = await Sentence.findById(audio.sentence);
+  for (const [index, audio] of audios.entries()) {
+    const { _id, link, voice, sentence: sentenceId, users } = audio;
+    const testId = audio.test.toString();
 
-          data.push({
-            ...audio,
-            email: userObject.email,
-            name: userObject.name,
-            point: Math.floor(user.point),
-            test: testObject.name,
-            sentence: sentenceObject.content,
-            listens: user.listens,
-          });
-        }),
-      );
-    }),
-  );
-  const dataReturned = data.map(a => {
-    delete a.users;
-    delete a.updatedAt;
-    delete a.averagePoint;
-    delete a.__v;
-    delete a.numberOfReviews;
-    return { ...a };
-  });
+    console.log((index + 1) / audios.length * 100);
 
-  await csvWriter.writeRecords(dataReturned);
+    let testName = testId === firstMosTest ? firstMosTest.name : secondMosTest.name;
 
-  // console.log({ dataReturned });
+    const pieceOfData = await Promise.all(users.map(async ({ userId, point, listens, lastUpdate }) => {
+      const user = await User.findById(userId);
+      const sentence = await Sentence.findById(sentenceId);
 
-  process.exit();
+      return {
+        test: testName,
+        audioId: _id,
+        link,
+        voice,
+        sentence: sentence.content,
+        listens,
+        email: user.email,
+        name: user.name,
+        point: Math.floor(point),
+        lastUpdatedAt: lastUpdate ? moment(lastUpdate).format('DD-MM-YYYY HH:mm') : '',
+      };
+    }));
+
+    data = [...data, ...pieceOfData];
+  }
+
+  fs.appendFileSync('scripts/mos.csv', 'test,audio_id,link,voice,sentence,listens,email,name,point,last_updated_at\n');
+  for (const item of data) {
+    fs.appendFileSync('scripts/mos.csv', Object.values(item).join(',') + '\n');
+  }
+
+  process.exit(1);
 })();
